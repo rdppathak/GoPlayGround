bfe2f2ab309802b822a2484506c439c03badd9a3
Rushikesh <rushikesh.pathak@nutanix.com>
2018-03-29 14:12:43 +0530

    Added variables and fmt examples

---
+++ GoInPractice/Chapter2/PackageFmt/PlayingWithFmt.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Printing something using GoLang")
+	var temp int
+	temp = 20
+	fmt.Printf("Priting float %.2f\n", float64(temp))
+}
